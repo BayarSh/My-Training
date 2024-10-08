cb3f27b7bc21f22c59a3379ceda5eb05ad1b9b42
BayarSh <bair.sharakshane@gmail.com>
2024-10-08 23:58:04 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+print('1st program: ',int(9**0.5*5))
+print('2nd program: ',9.99>9.98 and 1000!=1000.1)
+print('3rd program: ',2*2+2==2*(2+2))
+print('4th program: ',int((float('123.456')*10)%10))
+
+#старая версия
+print('\nСтарая версия:')
+print('1st program: ',int(9**0.5*5))
+print('2nd program: ',9.99>9.98 and 1000!=1000.1)
+print('3rd program: ',1234%1000//10+5678%1000//10)
+print('4th program: ',int(13.42)==42.13*100%100 or int(42.13)==13.42*100%100)
